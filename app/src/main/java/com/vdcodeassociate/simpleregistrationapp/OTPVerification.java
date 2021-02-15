feae21fbc1356dd35e972c9c9159080278b60544
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-02-15 19:01:02 +0530

    commit

--- app/src/main/java/com/vdcodeassociate/simpleregistrationapp/OTPVerification.java
+++ app/src/main/java/com/vdcodeassociate/simpleregistrationapp/OTPVerification.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class OTPVerification extends AppCompatActivity {
 
@@ -18,5 +19,6 @@ public class OTPVerification extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        Log.d(null, "onCreate: ");
     }
 }
